1bf351181455306e44ce1cc5f6d52a5b2f4a13ce
graceyudhaaa <graceyudha@students.unnes.ac.id>
2022-06-26 08:15:36 +0700

    profile route and forms

--- blog/app/blueprint/profile/controllers.py
+++ blog/app/blueprint/profile/controllers.py
@@ -1,14 +1,19 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
+from .forms import UpdateProfileForm
+
 profile = Blueprint(
     "profile", __name__, template_folder="templates", static_folder="static"
 )
 
 
-@profile.route("/profile")
+@profile.route("/profile", methods=["GET", "POST"])
 @login_required
 def index():
     username = current_user.username
     title = f"Profile {username}"
-    return render_template("profile.html", title=title)
+
+    form = UpdateProfileForm()
+
+    return render_template("profile.html", title=title, form=form)
---
+++ blog/app/blueprint/profile/forms.py
@@ -0,0 +1,28 @@
+from flask_jwt_extended import current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask import current_app
+
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=1, max=50)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Save Profile")
+
+    def validate_username(self, username):
+        if current_user.username != username.data:
+            if (
+                current_app.db["users"].count_documents({"username": username.data})
+                != 0
+            ):
+                raise ValidationError(
+                    f"username {username.data} already exists", "danger"
+                )
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            if current_app.db["users"].count_documents({"email": email.data}) != 0:
+                raise ValidationError(f"email {email.data} already exists", "danger")
